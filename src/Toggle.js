a9586d6dcbf70263eb8ac33eec09330374203aa2
Katie <Katie@Sandstone.local>
2020-03-08 09:47:43 +0900

    add location

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,38 +1,79 @@
 import React, { useState } from 'react';
-import { useSpring, animated } from 'react-spring';
+import { useTransition, animated } from 'react-spring';
 
 const Toggle = () => {
-  const [isToggled, setToggle] = useState(false);
-  const { color, y } = useSpring({
-    // opacity: isToggled ? 1 : 0,
-    // fontSize: isToggled ? '2rem' : '5em',
-    // transform: isToggled ? 'translated3d(0,0,0)' : 'translate3d(0,-50px,0)'
-    color: isToggled ? 'tomato' : 'green',
-    y: isToggled ? 0 : 1
+  const [items, setItems] = useState([
+    {
+      letter: 'K',
+      key: 1
+    },
+    {
+      letter: 'a',
+      key: 2
+    },
+    {
+      letter: 't',
+      key: 3
+    },
+    {
+      letter: 'i',
+      key: 4
+    },
+    {
+      letter: 'e',
+      key: 5
+    }
+  ]);
+  const transition = useTransition(items, item => item.key, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
   });
 
   return (
     <div>
-      <animated.h1
-        style={{
-          transform: y
-            .interpolate({
-              range: [0, 0.25, 0.5, 0.75, 1],
-              output: [0, -25, -50, -100, -50]
-            })
-            .interpolate(y => `translate3d(0, ${y}px, 0)`),
-          color
-        }}
+      {transition.map(({ item, key, props }) => (
+        <animated.h1 style={props} key={key}>
+          {item.letter}
+        </animated.h1>
+      ))}
+      <button
+        onClick={() =>
+          setItems([
+            {
+              letter: 'K',
+              key: 1
+            }
+          ])
+        }
       >
-        Hello
-      </animated.h1>
-      <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+        Toggle
+      </button>
     </div>
   );
 };
 
-//compomemt
-// const AnimatedTitle = animated.h1;
-// const AnimatedTitle = animated(componentName);
-
 export default Toggle;
+
+// const transition = useTransition(isToggled, null, {
+//   from: { opacity: 0, position: 'absolute' },
+//   enter: { opacity: 1 },
+//   leave: { opacity: 0 }
+// });
+
+/* <div>
+      style=
+      {{
+        position: 'relative'
+      }}
+    </div>
+    <div>
+{transition.map(({ item, key, props }) =>
+  item ? (
+    <animated.h1 style={props}>Hello</animated.h1>
+  ) : (
+    <animated.h1 style={props}>World</animated.h1>
+  )
+)}
+<button onClick={() => setToggle(!isToggled)}>Toggle</button>
+</div> */
